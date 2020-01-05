b0ca68824b1442090a3587f9dbd49e40517e70ad
Bill Paul <wpaul@windriver.com>
2020-01-05 14:04:10 -0800

    Add other missing modules

---
+++ software/firmware/badge/userfonts.h
@@ -0,0 +1,13 @@
+/* This is a list of user fonts added to the app
+ * 
+ * You can convert new fonts here: http://fonts.ugfx.io/
+ * 
+ * Be sure to update "fontlist.h" with the short names to keep us sane.
+ */
+
+#include "font-futura-medium-36.h" // anti aliased, very large
+#include "font-futura-medium-20.h" // anti aliased, general use
+#include "font-futura-medium-12.h" // anti aliased, small
+#include "font-manteka20.h"        // fixed font, might replace.
+#include "font-octin-stencil-24.h" // non-anti-alaised, game font
+
