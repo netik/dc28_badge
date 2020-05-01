0921c8985470445ec1ee203d72c5aac36e1f58d6
Bill Paul <wpaul@windriver.com>
2020-05-01 11:48:29 -0700

    Add preliminary SD card assets

---
+++ software/sd_card/cardgen.py
@@ -0,0 +1,14 @@
+import random
+
+suits = 'CDHS'
+cards = 'A23456789TJQK'
+deck = [ j + i for i in suits for j in cards ] + [ 'FJ', 'SJ' ]
+
+r = random.Random()
+r.seed(230987459872341398732)
+shuffled = ''.join(r.sample(deck, len(deck)))
+
+if __name__ == '__main__':
+	nperrow = 12
+	for i in xrange(0, len(shuffled), nperrow):
+		print shuffled[i:i + nperrow]
