53091e8269043f9ea9ca60da13abdbedc40c0f6b
Bill Paul <wpaul@windriver.com>
2020-05-01 11:45:23 -0700

    Add image conversion script

---
+++ software/tools/scripts/convert_to_rgb.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# SPQR/Ides of March Asset Build System
+# 1/2017
+#
+# John Adams <jna@retina.net> @netik
+# Bill Paul <wpaul@windriver.com>
+#
+# convert_to_rgb.sh
+#
+# Convert any readable image into a rgb565 image with proper header.
+# Requires ffmpeg, imagemagick, and rgbhdr
+#
+# Run this from chibios-orchard/sd_card only, or tools will not be
+# located.
+#
+
+MYDIR=`dirname "$0"`
+
+FFMPEG=ffmpeg
+IDENTIFY=identify
+RGBHDR=${MYDIR}/../bin/rgbhdr
+
+if [ "$1" == "" ]; then
+    echo "Usage: $0 filename"
+    exit
+fi
+
+filename=$1
+newfilename=${filename%.*}.rgb
+width=`${IDENTIFY} -format %w $filename`
+height=`${IDENTIFY} -format %h $filename`
+
+# create the rgb header
+${RGBHDR} ${width} ${height} /tmp/hdr$$.rgb
+CODEC=jpegls
+extension=.tif
+
+if [ "$filename" != "${filename%"$extension"*}" ];
+then
+    CODEC=tiff
+fi
+
+${FFMPEG} -loglevel panic -vcodec ${CODEC} -i $filename -vcodec rawvideo -f rawvideo -pix_fmt rgb565 /tmp/out$$.raw 
+
+if [ $? != 0 ]; then
+    echo "FFMPEG failed to decode ${filename} !"
+    exit 1
+fi
+
+
+cat /tmp/hdr$$.rgb /tmp/out$$.raw > ${newfilename}
+
+rm -f /tmp/hdr$$.rgb
+rm -f /tmp/out$$.raw
+
