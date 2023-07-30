57997658eb579fb3e9eebc82320d8598883e9590
Bill Paul <wpaul@windriver.com>
2023-07-30 15:17:02 -0700

    Fix the bg_black.sh script
    Apparently you need to use the magick(1) tool to extract pixel
    values now instead of the convert(1) tool.

--- software/tools/scripts/bg_black.sh
+++ software/tools/scripts/bg_black.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Find the color at pixel 0,0 and replace it with black.
 # This assumes the pixel at 0,0 is part of the background.
+set -x
 
-BG=`convert $1 -format '%[pixel:p{0,0}]' info:-`
-convert $1 -fuzz 7% -fill black -opaque $BG $2
+BG=`magick $1 -quiet -format '%[pixel:p{0,0}]' info:-`
+convert $1 -quiet -fuzz 7% -fill black -opaque $BG $2
